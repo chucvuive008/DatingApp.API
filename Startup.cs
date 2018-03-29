40a2a9c184a5ff422966db4f18f526052fafb022
008 <tom.vuong008@gmail.com>
2018-03-29 03:34:52 -0500

    AddCors

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +37,8 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            //allow browser
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials()); //because this is not production we can allow all
             app.UseMvc();
         }
     }
